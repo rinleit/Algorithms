3e46df192932b0da99bdee51c239ea29399bd95e
root <root@dweber-T420.(none)>
2013-10-25 00:31:05 -0400

    Syntax corrections

--- Search/Python/DepthFirstSearch.py
+++ Search/Python/DepthFirstSearch.py
@@ -1,15 +1,15 @@
 class DepthFirstSearch:
 
     def dfs(self, graph, start):
-        stack = [start]
+        verticies = [start]
         path = []
-        while not stack.isEmpty:
-            vertex = stack.pop()
+        while verticies:
+            vertex = verticies.pop()
             if vertex not in path:
                 path.append(vertex)
             for node in reversed(graph[vertex]):
                 if node not in path:
-                    stack.append(node)
+                    verticies.append(node)
         return path
 
     def dfs_recursive(self, graph, start, path = []):
@@ -22,10 +22,10 @@ class DepthFirstSearch:
 if __name__ == '__main__':
     dfs = DepthFirstSearch()
     graph = {1: [2, 3],
-             2: [1, 4, 6],
+             2: [1, 4, 5, 6],
              3: [1, 4],
              4: [2, 3, 5],
              5: [2, 4, 6],
-             6: [2, 5, 9]}
+             6: [2, 5]}
     print("DFS: ", dfs.dfs(graph, 1))
     print("DFS recursive: ", dfs.dfs_recursive(graph, 1))
