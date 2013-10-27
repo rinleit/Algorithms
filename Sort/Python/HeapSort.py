172765d25d2c8da049d5c3b7b156c6edeaa3a9e4
Dave Weber <weberd09@gmail.com>
2013-10-26 22:59:15 -0400

    Starter code for heap and radix sort

---
+++ Sort/Python/HeapSort.py
@@ -0,0 +1,8 @@
+class HeapSort:
+
+    def heap_sort(self, array):
+
+if __name__ == '__main__':
+    hs = HeapSort()
+    array = [4, 65, 2, -31, 0, 99, 83, 782, 1]
+    print(hs.heap_sort(array))
---
+++ Sort/Python/RadixSort.py
@@ -0,0 +1,8 @@
+class RadixSort:
+
+    def radix_sort(self, array):
+
+if __name__ == '__main__':
+    rs = RadixSort()
+    array = [4, 65, 2, -31, 0, 99, 83, 782, 1]
+    print(rs.radix_sort(array))
