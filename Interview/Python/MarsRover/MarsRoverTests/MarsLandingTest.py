d872535c791e137aa9d7185ed1cf782c655b2926
Dave Weber <weberd09@gmail.com>
2014-06-06 16:59:08 -0400

    Further main class testing

--- Interview/Python/MarsRover/MarsRoverTests/MarsLandingTest.py
+++ Interview/Python/MarsRover/MarsRoverTests/MarsLandingTest.py
@@ -1,30 +1,43 @@
 import unittest
 import MarsLanding
+import Plateau
+from InputErrors import *
 
 
 class MarsLandingTest(unittest.TestCase):
 
+    mars_landing = MarsLanding.MarsLanding()
+    plateau = Plateau.Plateau(5, 5)
+
     def testCreateNormalPlateu(self):
-        return NotImplemented
+        plateau = self.mars_landing.create_plateau('5 5')
+        self.assertTrue(plateau.max_x, 5)
+        self.assertTrue(plateau.max_y, 5)
 
     def testInvalidPlateu(self):
-        return NotImplemented
-
-
+        self.assertRaises(PlateauInputError,
+                          self.mars_landing.create_plateau,
+                          'a 5')
 
     def testCreateNormalRover(self):
-        return NotImplemented
+        rover = self.mars_landing.create_rover('1 1 N', 'LRM', self.plateau)
+        self.assertEquals(rover.x, 1)
+        self.assertEquals(rover.y, 1)
+        self.assertEquals(rover.orientation, 'N')
+        self.assertEquals(rover.commands, 'LRM')
+        self.assertEquals(rover.plateau, self.plateau)
 
     def testBadPositionRover(self):
-        return NotImplemented
+        rover = self.mars_landing.create_rover('1 1 A', 'LRM', self.plateau)
+        self.assertIsNone(rover)
 
     def testInvalidPositionRover(self):
-        return NotImplemented
+        rover = self.mars_landing.create_rover('6 6 N', 'LRM', self.plateau)
+        self.assertIsNone(rover)
 
     def testBadCommandsRover(self):
-        return NotImplemented
-
-
+        rover = self.mars_landing.create_rover('1 1 N', 'LRMA', self.plateau)
+        self.assertIsNone(rover)
 
     def testEmptyInput(self):
         return NotImplemented
@@ -40,7 +53,7 @@ class MarsLandingTest(unittest.TestCase):
 
     def testMultiRoverSquad(self):
         return NotImplemented
-    
+
 
 def main():
     unittest.main()
