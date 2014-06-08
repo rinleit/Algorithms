eaeeea24c57cadd6cdfc778f8d57a22a394e72b8
Dave Weber <weberd09@gmail.com>
2014-06-08 16:19:11 -0400

    Plateau change
    Change to private method

--- Interview/Python/MarsRover/Plateau.py
+++ Interview/Python/MarsRover/Plateau.py
@@ -5,8 +5,9 @@ class Plateau:
         self.max_y = int(y)
 
     def is_valid_position(self, x, y):
-        return self.is_valid_coordinate(self.max_x, x) and \
-               self.is_valid_coordinate(self.max_y, y)
+        return self.__is_valid_coordinate(self.max_x, x) and \
+               self.__is_valid_coordinate(self.max_y, y)
 
     @staticmethod
+    def __is_valid_coordinate(max_coordinate, coordinate):
         return 0 <= coordinate <= max_coordinate
