7d964156ef7f79c816cc6bfbdb034bd1b6c75707
Dave Weber <weberd09@gmail.com>
2014-06-19 17:27:13 -0400

    FileReader
    Windows/Mac file reading changes, form binding

--- Interview/Python/fileReader/fileReader/app/views.py
+++ Interview/Python/fileReader/fileReader/app/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 import csv
+import codecs
 
 from fileReader.app.forms import UploadFileForm
 from fileReader.app.models import CompanyData
@@ -28,15 +29,23 @@ def home(request):
 
 
 def handle_uploaded_file(f):
-    dataReader = csv.reader(f, delimiter=',', quotechar='"')
-    for row in dataReader:
-        companyData = CompanyData
-        companyData.date = row[0]
-        companyData.category = row[1]
-        companyData.employee_name = row[2]
-        companyData.employee_address = row[3]
-        companyData.expense_description = row[4]
-        companyData.pre_tax_amount = row[5]
-        companyData.tax_name = row[6]
-        companyData.tax_amount = row[7]
-        print("%s %s" % (companyData.date, companyData.category))
+    csv_data = csv.reader(codecs.iterdecode(f, 'utf-8'), delimiter=',', quotechar='"')
+    #csv_data = csv.reader(f, delimiter=',', quotechar='"')
+    for row in csv_data:
+
+        if row == ['date', 'category', 'employee name', 'employee address', 'expense description', 'pre-tax amount', 'tax name', 'tax amount']:
+            pass
+        else:
+            print(row)
+            date = row[0]
+            category = row[1]
+            employee_name = row[2]
+            employee_address = row[3]
+            expense_description = row[4]
+            pre_tax_amount = row[5]
+            tax_name = row[6]
+            tax_amount = row[7]
+
+            company_data = CompanyData(date, category, employee_name, employee_address,
+                                       expense_description, pre_tax_amount, tax_name, tax_amount)
+            company_data.save()
