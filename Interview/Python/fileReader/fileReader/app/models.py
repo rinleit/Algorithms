0881e0c81a2bf1ba047b0c7f83dc4d7828de717f
Dave Weber <weberd09@gmail.com>
2014-06-20 01:39:30 -0400

    FileReader Models
    Fixed model, view for saving to DB, writing to DB properly

--- Interview/Python/fileReader/fileReader/app/models.py
+++ Interview/Python/fileReader/fileReader/app/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-import datetime
 
 
 class CompanyData(models.Model):
-    date = models.CharField(max_length=5)
+    date = models.DateField()
     category = models.CharField(max_length=5)
     employee_name = models.CharField(max_length=5)
     employee_address = models.CharField(max_length=5)
     expense_description = models.CharField(max_length=5)
-    pre_tax_amount = models.CharField(max_length=5)
+    pre_tax_amount = models.DecimalField(max_digits=8, decimal_places=2)
     tax_name = models.CharField(max_length=5)
-    tax_amount = models.CharField(max_length=5)
+    tax_amount = models.DecimalField(max_digits=8, decimal_places=2)
--- Interview/Python/fileReader/fileReader/app/views.py
+++ Interview/Python/fileReader/fileReader/app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from datetime import datetime
+from decimal import *
 import csv
 import codecs
 
@@ -30,22 +32,14 @@ def home(request):
 
 def handle_uploaded_file(f):
     csv_data = csv.reader(codecs.iterdecode(f, 'utf-8'), delimiter=',', quotechar='"')
-    #csv_data = csv.reader(f, delimiter=',', quotechar='"')
+    next(csv_data)
     for row in csv_data:
-
-        if row == ['date', 'category', 'employee name', 'employee address', 'expense description', 'pre-tax amount', 'tax name', 'tax amount']:
-            pass
-        else:
-            print(row)
-            date = row[0]
-            category = row[1]
-            employee_name = row[2]
-            employee_address = row[3]
-            expense_description = row[4]
-            pre_tax_amount = row[5]
-            tax_name = row[6]
-            tax_amount = row[7]
-
-            company_data = CompanyData(date, category, employee_name, employee_address,
-                                       expense_description, pre_tax_amount, tax_name, tax_amount)
-            company_data.save()
+        company_data = CompanyData(date=datetime.strptime(row[0], "%m/%d/%Y").date(),
+                                   category=row[1],
+                                   employee_name=row[2],
+                                   employee_address=row[3],
+                                   expense_description=row[4],
+                                   pre_tax_amount=Decimal(row[5].strip().replace(',', '')),
+                                   tax_name=row[6],
+                                   tax_amount=Decimal(row[7].strip().replace(',', '')))
+        company_data.save()
