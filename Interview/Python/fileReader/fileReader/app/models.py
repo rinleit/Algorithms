33d6e868ab9771b374fcfd1d5658b11e705c8636
Dave Weber <weberd09@gmail.com>
2014-06-23 16:55:33 -0400

    FileReader
    Add header, tests

--- Interview/Python/fileReader/fileReader/app/models.py
+++ Interview/Python/fileReader/fileReader/app/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class CompanyData(models.Model):
     date = models.DateField()
-    category = models.CharField(max_length=5)
-    employee_name = models.CharField(max_length=5)
-    employee_address = models.CharField(max_length=5)
-    expense_description = models.CharField(max_length=5)
+    category = models.CharField(max_length=256)
+    employee_name = models.CharField(max_length=256)
+    employee_address = models.CharField(max_length=256)
+    expense_description = models.CharField(max_length=256)
     pre_tax_amount = models.DecimalField(max_digits=8, decimal_places=2)
-    tax_name = models.CharField(max_length=5)
+    tax_name = models.CharField(max_length=256)
     tax_amount = models.DecimalField(max_digits=8, decimal_places=2)
---
+++ Interview/Python/fileReader/fileReader/tests.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+from fileReader.app.models import CompanyData
+
+
+class CompanyDataTests(TestCase):
+
+    def test_normal_creation(self):
+        """
+        description
+        """
+        pass
+
+    def test_bad_date_company_data(self):
+        """
+        description
+        """
+        pass
+
+    def test_bad_tax_company_data(self):
+        """
+        description
+        """
+        pass
